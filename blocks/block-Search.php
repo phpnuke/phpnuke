d2dade231b0970d63640e8d4a30bb977f54d9ee2
Meisam Nobari <meis2m@yahoo.com>
2021-03-29 17:20:27 +0430

    Update block-Search.php

--- blocks/block-Search.php
+++ blocks/block-Search.php
@@ -1,28 +1,24 @@
 <?php
+/**
+ *
+ * This file is part of the PHP-NUKE Software package.
+ *
+ * @copyright (c) PHP-NUKE <https://www.phpnuke.ir>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
 
-/************************************************************************/
-/* PHP-NUKE: Web Portal System                                          */
-/* Part: blocks				                                            */
-/* Part Name: block-ads		                                            */
-/* ===========================                                          */
-/*                                                                      */
-/* Copyright (c) 2006 by Francisco Burzi                                */
-/* http://phpnuke.org                                                   */
-/*                                                                      */
-/* This program is free software. You can redistribute it and/or modify */
-/* it under the terms of the GNU General Public License as published by */
-/* the Free Software Foundation; either version 2 of the License.       */
-/************************************************************************/
-
-if (!defined('BLOCK_FILE'))
-{
+if (!defined('BLOCK_FILE')) {
     Header("Location: ../index.php");
     die();
 }
 
 global $align;
-$content = "
-<form action=\"".LinkToGT("index.php?modname=Search")."\" method=\"post\" role=\"search\">
+$content =
+    "
+<form action=\"" .
+    LinkToGT("index.php?modname=Search") .
+    "\" method=\"post\" role=\"search\">
 <div class=\"form-group input-group text-center\">
 	<input type=\"text\" class=\"form-control\" placeholder=\"Search..\" value=\"\" name=\"search_query\">
 	<span class=\"input-group-btn\">
@@ -31,7 +27,9 @@ $content = "
 		</button>
 	</span>
 </div>
-<input type=\"hidden\" name=\"csrf_token\" value=\""._PN_CSRF_TOKEN."\" /> 
+<input type=\"hidden\" name=\"csrf_token\" value=\"" .
+    _PN_CSRF_TOKEN .
+    "\" /> 
 </form>";
 
 ?>
