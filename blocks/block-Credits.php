8514379dc7ed9a65f90dd9bb05a64deddfbd6446
Meisam Nobari <meis2m@yahoo.com>
2021-03-29 17:16:06 +0430

    Update block-Credits.php

--- blocks/block-Credits.php
+++ blocks/block-Credits.php
@@ -1,21 +1,14 @@
 <?php
+/**
+ *
+ * This file is part of the PHP-NUKE Software package.
+ *
+ * @copyright (c) PHP-NUKE <https://www.phpnuke.ir>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
 
-/************************************************************************/
-/* PHP-NUKE: Web Portal System                                          */
-/* Part: blocks				                                            */
-/* Part Name: block-ads		                                            */
-/* ===========================                                          */
-/*                                                                      */
-/* Copyright (c) 2006 by Francisco Burzi                                */
-/* http://phpnuke.org                                                   */
-/*                                                                      */
-/* This program is free software. You can redistribute it and/or modify */
-/* it under the terms of the GNU General Public License as published by */
-/* the Free Software Foundation; either version 2 of the License.       */
-/************************************************************************/
-
-if (!defined('BLOCK_FILE'))
-{
+if (!defined('BLOCK_FILE')) {
     Header("Location: ../index.php");
     die();
 }
@@ -24,16 +17,30 @@ global $db, $nuke_configs, $userinfo;
 
 $content = "";
 
-if(isset($userinfo['is_registered']) && $userinfo['is_registered'] == 1)
-{
-	$content = "
+if (isset($userinfo['is_registered']) && $userinfo['is_registered'] == 1) {
+    $content =
+        "
 	<div class=\"text-center\">
-	"._CREDITS_REMAIN."<br /><b>".number_format(user_credits_allowed($userinfo['user_id']))." "._RIAL."</b>
+	" .
+        _CREDITS_REMAIN .
+        "<br /><b>" .
+        number_format(user_credits_allowed($userinfo['user_id'])) .
+        " " .
+        _RIAL .
+        "</b>
 	</div>
 	<div class=\"text-justify\">
 		<ul>
-			<li><a href=\"".LinkToGT("index.php?modname=Credits&op=credits_list")."\">"._CREDITS_LIST."</a></li>
-			<li><a href=\"".LinkToGT("index.php?modname=Credits")."\">"._CREDITS_CHARGE."</a></li>
+			<li><a href=\"" .
+        LinkToGT("index.php?modname=Credits&op=credits_list") .
+        "\">" .
+        _CREDITS_LIST .
+        "</a></li>
+			<li><a href=\"" .
+        LinkToGT("index.php?modname=Credits") .
+        "\">" .
+        _CREDITS_CHARGE .
+        "</a></li>
 		</ul>
 	</div>";
 }
