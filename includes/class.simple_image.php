575f5d44aad6ca470e8defc06a8aca8c3f3fcfbf
Meisam Nobari <meis2m@yahoo.com>
2021-03-30 19:17:31 +0430

    Update class.simple_image.php

--- includes/class.simple_image.php
+++ includes/class.simple_image.php
@@ -1,150 +1,159 @@
 <?php
+/**
+ *
+ * This file is part of the PHP-NUKE Software package.
+ *
+ * @copyright (c) PHP-NUKE <https://www.phpnuke.ir>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
 
-if(!defined('NUKE_FILE'))
-{
-	die ("You can't access this file directly...");
+if (!defined('NUKE_FILE')) {
+    die("You can't access this file directly...");
 }
 
-class SimpleImage {
-
+class SimpleImage
+{
     var $image;
     var $image_type;
     var $compression;
 
     function load($data_image = false, $filename, $mime)
-	{
-        if ($data_image)
-		{
+    {
+        if ($data_image) {
             $this->image = imagecreatefromstring($filename);
             $this->image_type = $mime;
-        }
-		else
-		{
+        } else {
             $image_info = getimagesize($filename);
             $this->image_type = $image_info[2];
-            if ($this->image_type == IMAGETYPE_JPEG)
-			{
-
+            if ($this->image_type == IMAGETYPE_JPEG) {
                 $this->image = imagecreatefromjpeg($filename);
-            }
-            elseif($this->image_type == IMAGETYPE_GIF)
-			{
-
+            } elseif ($this->image_type == IMAGETYPE_GIF) {
                 $this->image = imagecreatefromgif($filename);
-            }
-            elseif($this->image_type == IMAGETYPE_PNG)
-			{
-
+            } elseif ($this->image_type == IMAGETYPE_PNG) {
                 $this->image = imagecreatefrompng($filename);
             }
         }
     }
 
-    function save($filename, $image_type = IMAGETYPE_JPEG, $compression = 75, $permissions = null)
-	{
-
-        if ($image_type == IMAGETYPE_JPEG)
-		{
+    function save(
+        $filename,
+        $image_type = IMAGETYPE_JPEG,
+        $compression = 75,
+        $permissions = null
+    ) {
+        if ($image_type == IMAGETYPE_JPEG) {
             imagejpeg($this->image, $filename, $compression);
-        }
-        elseif($image_type == IMAGETYPE_GIF)
-		{
-
+        } elseif ($image_type == IMAGETYPE_GIF) {
             imagegif($this->image, $filename);
-        }
-        elseif($image_type == IMAGETYPE_PNG)
-		{
-
+        } elseif ($image_type == IMAGETYPE_PNG) {
             imagepng($this->image, $filename);
         }
-        if ($permissions != null)
-		{
-
+        if ($permissions != null) {
             chmod($filename, $permissions);
         }
     }
 
     function output($image_type = IMAGETYPE_JPEG)
-	{
-
-        if ($image_type == IMAGETYPE_JPEG)
-		{
+    {
+        if ($image_type == IMAGETYPE_JPEG) {
             imagejpeg($this->image, null, $this->compression);
-        }
-        elseif($image_type == IMAGETYPE_GIF)
-		{
-
+        } elseif ($image_type == IMAGETYPE_GIF) {
             imagegif($this->image, null, $this->compression);
-        }
-        elseif($image_type == IMAGETYPE_PNG)
-		{
-
+        } elseif ($image_type == IMAGETYPE_PNG) {
             imagepng($this->image, null, $this->compression);
         }
     }
 
     function getWidth()
-	{
-
+    {
         return imagesx($this->image);
     }
 
     function getHeight()
-	{
-
+    {
         return imagesy($this->image);
     }
 
     function resizeToHeight($height)
-	{
-
+    {
         $ratio = $height / $this->getHeight();
         $width = $this->getWidth() * $ratio;
         $this->resize($width, $height);
     }
 
     function resizeToWidth($width)
-	{
+    {
         $ratio = $width / $this->getWidth();
         $height = $this->getheight() * $ratio;
         $this->resize($width, $height);
     }
 
-    function scale($scale) {
-        $width = $this->getWidth() * $scale / 100;
-        $height = $this->getheight() * $scale / 100;
+    function scale($scale)
+    {
+        $width = ($this->getWidth() * $scale) / 100;
+        $height = ($this->getheight() * $scale) / 100;
         $this->resize($width, $height);
     }
 
     function resize($width, $height)
-	{
+    {
         $new_image = imagecreatetruecolor($width, $height);
-        imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+        imagecopyresampled(
+            $new_image,
+            $this->image,
+            0,
+            0,
+            0,
+            0,
+            $width,
+            $height,
+            $this->getWidth(),
+            $this->getHeight()
+        );
         $this->image = $new_image;
     }
 
     function resizeTransparentImage($width, $height)
-	{
+    {
         $new_image = imagecreatetruecolor($width, $height);
-        if ($this->image_type == IMAGETYPE_GIF || $this->image_type == IMAGETYPE_PNG)
-		{
+        if (
+            $this->image_type == IMAGETYPE_GIF ||
+            $this->image_type == IMAGETYPE_PNG
+        ) {
             $current_transparent = imagecolortransparent($this->image);
-            if ($current_transparent != -1)
-			{
-                $transparent_color = imagecolorsforindex($this->image, $current_transparent);
-                $current_transparent = imagecolorallocate($new_image, $transparent_color['red'], $transparent_color['green'], $transparent_color['blue']);
+            if ($current_transparent != -1) {
+                $transparent_color = imagecolorsforindex(
+                    $this->image,
+                    $current_transparent
+                );
+                $current_transparent = imagecolorallocate(
+                    $new_image,
+                    $transparent_color['red'],
+                    $transparent_color['green'],
+                    $transparent_color['blue']
+                );
                 imagefill($new_image, 0, 0, $current_transparent);
                 imagecolortransparent($new_image, $current_transparent);
-            }
-            elseif($this->image_type == IMAGETYPE_PNG)
-			{
+            } elseif ($this->image_type == IMAGETYPE_PNG) {
                 imagealphablending($new_image, false);
                 $color = imagecolorallocatealpha($new_image, 0, 0, 0, 127);
                 imagefill($new_image, 0, 0, $color);
                 imagesavealpha($new_image, true);
             }
         }
-        imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+        imagecopyresampled(
+            $new_image,
+            $this->image,
+            0,
+            0,
+            0,
+            0,
+            $width,
+            $height,
+            $this->getWidth(),
+            $this->getHeight()
+        );
         $this->image = $new_image;
     }
 }
