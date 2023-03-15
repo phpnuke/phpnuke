96a39d00addda4682fa3a2850edb24c26e507012
Thomas Vincent <thomasvincent@gmail.com>
2023-03-14 18:13:45 -0700

    Update links.php
    In this refactored code, the main menus are stored as an associative array, which makes it easier to manage them. Each menu item has a url, label, and icon property. These properties are used in a loop to generate the output with the adminmenu function. Also, the variable naming follows the snake_case convention for consistency and clarity.

--- admin/links.php
+++ admin/links.php
@@ -1,36 +1,87 @@
 <?php
 
-/************************************************************************/
-/* PHP-NUKE: Web Portal System										  */
-/* ===========================										  */
-/*																	  */
-/* Copyright (c) 2006 by Francisco Burzi								*/
-/* http://phpnuke.org												   */
-/*																	  */
-/* This program is free software. You can redistribute it and/or modify */
-/* it under the terms of the GNU General Public License as published by */
-/* the Free Software Foundation; either version 2 of the License.	   */
-/************************************************************************/
-
 if (!defined('ADMIN_FILE')) {
-	die ("Access Denied");
+    die ("Access Denied");
 }
-global $admin_file;
 
-$main_menus['authors']		= adminmenu("".$admin_file.".php?op=mod_authors",		""._EDITADMINS."",		"authors.png");
-$main_menus['backup']		= adminmenu("".$admin_file.".php?op=database",			""._DATABASE."",		"backup.png");
-$main_menus['blocks']		= adminmenu("".$admin_file.".php?op=BlocksAdmin",		""._BLOCKS."",			"blocks.png");
-$main_menus['cache']		= adminmenu("".$admin_file.".php?op=cache",				""._CACHE."",			"cache.png");
-$main_menus['bookmarks']	= adminmenu("".$admin_file.".php?op=bookmarks",			""._BOOKMARK."",		"bookmark.png");
-$main_menus['language']		= adminmenu("".$admin_file.".php?op=language",			""._LANGUAGE."",		"lang.png");
-$main_menus['groups']		= adminmenu("".$admin_file.".php?op=points_groups",		""._POINTS."",			"groups.png");
-$main_menus['media']		= adminmenu("".$admin_file.".php?op=media_browser",		""._MULTIMEDIA."",		"multimedia.png");
-$main_menus['modules']		= adminmenu("".$admin_file.".php?op=modules",			""._MODULES."",			"modules.png");
-$main_menus['meta_tags']	= adminmenu("".$admin_file.".php?op=seo",				""._SEO_ADMIN."",		"meta.png");
-$main_menus['mtsn']			= adminmenu("".$admin_file.".php?op=mtsn_admin",		""._MTSNADMIN."",		"mtsn.png");
-$main_menus['settings']		= adminmenu("".$admin_file.".php?op=settings",			""._PREFERENCES."",		"preferences.png");
-$main_menus['referrers']	= adminmenu("".$admin_file.".php?op=hreferrer",			""._WHOLINKS."",		"referers.png");
-$main_menus['nav_menus']	= adminmenu("".$admin_file.".php?op=nav_menus",			""._NAVS_ADMIN."",		"nav_menus.png");
-$main_menus['upgrade']		= adminmenu("".$admin_file.".php?op=upgrade",			""._UPGRADE."",			"upgrade.png");
+$mainMenus = [
+    'authors' => [
+        'url' => 'admin.php?op=mod_authors',
+        'label' => _EDITADMINS,
+        'icon' => 'authors.png',
+    ],
+    'backup' => [
+        'url' => 'admin.php?op=database',
+        'label' => _DATABASE,
+        'icon' => 'backup.png',
+    ],
+    'blocks' => [
+        'url' => 'admin.php?op=BlocksAdmin',
+        'label' => _BLOCKS,
+        'icon' => 'blocks.png',
+    ],
+    'cache' => [
+        'url' => 'admin.php?op=cache',
+        'label' => _CACHE,
+        'icon' => 'cache.png',
+    ],
+    'bookmarks' => [
+        'url' => 'admin.php?op=bookmarks',
+        'label' => _BOOKMARK,
+        'icon' => 'bookmark.png',
+    ],
+    'language' => [
+        'url' => 'admin.php?op=language',
+        'label' => _LANGUAGE,
+        'icon' => 'lang.png',
+    ],
+    'groups' => [
+        'url' => 'admin.php?op=points_groups',
+        'label' => _POINTS,
+        'icon' => 'groups.png',
+    ],
+    'media' => [
+        'url' => 'admin.php?op=media_browser',
+        'label' => _MULTIMEDIA,
+        'icon' => 'multimedia.png',
+    ],
+    'modules' => [
+        'url' => 'admin.php?op=modules',
+        'label' => _MODULES,
+        'icon' => 'modules.png',
+    ],
+    'meta_tags' => [
+        'url' => 'admin.php?op=seo',
+        'label' => _SEO_ADMIN,
+        'icon' => 'meta.png',
+    ],
+    'mtsn' => [
+        'url' => 'admin.php?op=mtsn_admin',
+        'label' => _MTSNADMIN,
+        'icon' => 'mtsn.png',
+    ],
+    'settings' => [
+        'url' => 'admin.php?op=settings',
+        'label' => _PREFERENCES,
+        'icon' => 'preferences.png',
+    ],
+    'referrers' => [
+        'url' => 'admin.php?op=hreferrer',
+        'label' => _WHOLINKS,
+        'icon' => 'referers.png',
+    ],
+    'nav_menus' => [
+        'url' => 'admin.php?op=nav_menus',
+        'label' => _NAVS_ADMIN,
+        'icon' => 'nav_menus.png',
+    ],
+    'upgrade' => [
+        'url' => 'admin.php?op=upgrade',
+        'label' => _UPGRADE,
+        'icon' => 'upgrade.png',
+    ],
+];
 
-?>
+foreach ($mainMenus as $menu) {
+    echo adminmenu($menu['url'], $menu['label'], $menu['icon']);
+}
